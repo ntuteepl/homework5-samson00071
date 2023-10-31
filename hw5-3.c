e079eaa3a35577125a8b5da673b85d594ea42979
samson00071 <91417049+samson00071@users.noreply.github.com>
2023-10-31 15:23:24 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -6,16 +6,16 @@ int main()
     int hours,salary;
     double money;
 
-  
+
     scanf("%d%d",&hours,&salary);
      money=hours*salary;
     if(hours>60 && hours<=120)
          money=60*salary+(hours-60)*1.33*salary;
     if(hours>120)
-    money=60*salary+60*salary*1.33+(hours-120)*1.66;
+    money=60*salary+60*salary*1.33+(hours-120)*salary*1.66;
 
 
-   
+
 
     printf("%.1f\n",money);
     return 0;
