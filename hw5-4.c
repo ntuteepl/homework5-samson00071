c502faa1208ec17cead3eb4f38a47a69d4d845fb
samson00071 <91417049+samson00071@users.noreply.github.com>
2023-10-31 15:04:10 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -6,7 +6,7 @@ int main()
     int h,m;
     double angle;
 
-    printf("輸入 時、分\n");
+
     scanf("%d%d",&h,&m);
     angle=(h*30+0.2*m)-m*6;
    if(1>h || h>12 )
@@ -19,9 +19,8 @@ int main()
     if(angle>180)
         angle=360-angle;
 
-    printf("角度%.1f\n",angle);
+    printf("%.1f\n",angle);
     return 0;
     exit:
-      printf("輸入錯誤\n");
-}
 
+}
