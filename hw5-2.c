389179b651c62b91eb04ca33a80637c090057445
samson00071 <91417049+samson00071@users.noreply.github.com>
2023-10-31 15:01:51 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int min;
+    double money,discount;
+    discount=1;
+    printf("請輸入通話分鐘數\n");
+    scanf("%d",&min);
+    if(money>800 && money<=1500)
+        discount=0.9;
+    if(money>1500)
+        discount=0.79;
+
+
+    money=min*0.9*discount;
+
+    printf("通話費%.1f\n",money);
+    return 0;
+}
 
